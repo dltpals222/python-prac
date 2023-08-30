c3e4276c29c9fe171060d3906049c2cc30f6a307
leesemin <leesemin222@gmail.com>
2023-08-30 18:42:20 +0900

    책에 테스트 서버 여는 방법이 모호하여 패스

--- server.py
+++ server.py
@@ -1,8 +1,26 @@
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode 
 
+#* Get 처리
 # f = urlopen("http://www.example.com")
 # print(f.read(500).decode('utf-8'))
 
-data = "language=python&framework=django"
-f = urlopen("http://127.0.0.1:8000", bytes(data, encoding='utf-8'))
+#* POST방식 요청
+# data = "language=python&framework=django"
+# f = urlopen("http://127.0.0.1:8000", bytes(data, encoding='utf-8'))
+# print(f.read(500).decode('utf-8'))
+
+#* Request 클래스로 요청 헤더 지정
+url = "http://127.0.0.1:8000"
+data = {
+    'name' : "BlahBlah",
+    'email' : "blah@example.com",
+    'url' : "http://www.naver.com"
+}
+encData = urlencode(data)
+postData = bytes(encData, encoding='utf-8')
+req = Request(url, data=postData)
+req.add_header('Content-Type', 'application/x-www-form-urlencoded')
+f = urlopen(req)
+print(f.info())
 print(f.read(500).decode('utf-8'))
