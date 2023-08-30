d4ac0242d52884d703c96d067bf71f80c03747ed
leesemin <leesemin222@gmail.com>
2023-08-30 20:22:38 +0900

    첫번째 요청 후 두번째 요청 시도

--- httpClientGet.py
+++ httpClientGet.py
@@ -1,6 +1,6 @@
 from http.client import HTTPConnection
 
-#* 첫번쨰 요청
+#* 첫번째 요청
 host = 'www.example.com'
 conn = HTTPConnection(host)
 conn.request('GET','/')
@@ -8,3 +8,13 @@ r1 = conn.getresponse()
 print(r1.status, r1.reason)
 
 data1 = r1.read()
+
+#* 두번째 요청
+conn.request('GET', '/')
+r2 = conn.getresponse()
+print(r2.status, r2.reason)
+
+data2 = r2.read()
+print(data2.decode())
+
+conn.close()
